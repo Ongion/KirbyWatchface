54ceef3e01a9dbdb1784b26fe0addd2a6780e9e3
Benjamin Paul <bepaul@microsoft.com>
2025-08-15 21:33:25 -0400

    Adjust positioning to center things.

--- src/chalk/animations.c
+++ src/chalk/animations.c
@@ -2,39 +2,39 @@
 
 const AbilityAnimation BEAM_ANIMATIONS[] =
 {
-	{.type = AT_APNG, .animation.APNGAnimation = {RESOURCE_ID_KIRBY_BEAM_ANIM, {0, 53}}}
+	{.type = AT_APNG, .animation.APNGAnimation = {RESOURCE_ID_KIRBY_BEAM_ANIM, {18, 53}}}
 };
 
 const AbilityAnimation CUTTER_ANIMATIONS[] =
 {
-	{.type = AT_APNG, .animation.APNGAnimation = {RESOURCE_ID_KIRBY_CUTTER_THROW_ANIM, {1, 82}}}
+	{.type = AT_APNG, .animation.APNGAnimation = {RESOURCE_ID_KIRBY_CUTTER_THROW_ANIM, {19, 82}}}
 };
 
 const AbilityAnimation FIRE_ANIMATIONS[] =
 {
-	{.type = AT_APNG, .animation.APNGAnimation = {RESOURCE_ID_KIRBY_FIRE_ANIM, {0, 67}}}
+	{.type = AT_APNG, .animation.APNGAnimation = {RESOURCE_ID_KIRBY_FIRE_ANIM, {18, 67}}}
 };
 
 const AbilityAnimation HAMMER_ANIMATIONS[] =
 {
-	{.type = AT_APNG, .animation.APNGAnimation = {RESOURCE_ID_KIRBY_HAMMER_POUND_ANIM, {5, 69}}}
+	{.type = AT_APNG, .animation.APNGAnimation = {RESOURCE_ID_KIRBY_HAMMER_POUND_ANIM, {23, 69}}}
 };
 
 const AbilityAnimation MIKE_ANIMATIONS[] =
 {
-	{.type = AT_APNG, .animation.APNGAnimation = {RESOURCE_ID_KIRBY_MIKE_ANIM_1, {0, 80}}},
-	{.type = AT_APNG, .animation.APNGAnimation = {RESOURCE_ID_KIRBY_MIKE_ANIM_2, {2, 84}}},
-	{.type = AT_APNG, .animation.APNGAnimation = {RESOURCE_ID_KIRBY_MIKE_ANIM_3, {5, 52}}},
+	{.type = AT_APNG, .animation.APNGAnimation = {RESOURCE_ID_KIRBY_MIKE_ANIM_1, {18, 80}}},
+	{.type = AT_APNG, .animation.APNGAnimation = {RESOURCE_ID_KIRBY_MIKE_ANIM_2, {20, 84}}},
+	{.type = AT_APNG, .animation.APNGAnimation = {RESOURCE_ID_KIRBY_MIKE_ANIM_3, {23, 52}}},
 };
 
 const AbilityAnimation SLEEP_ANIMATIONS[] =
 {
-	{.type = AT_APNG, .animation.APNGAnimation = {RESOURCE_ID_KIRBY_SLEEP_ANIM, {11, 59}}}
+	{.type = AT_APNG, .animation.APNGAnimation = {RESOURCE_ID_KIRBY_SLEEP_ANIM, {29, 59}}}
 };
 
 const AbilityAnimation SWORD_ANIMATIONS[] =
 {
-	{.type = AT_APNG, .animation.APNGAnimation = {RESOURCE_ID_KIRBY_SWORD_ANIM, {0, 69}}}
+	{.type = AT_APNG, .animation.APNGAnimation = {RESOURCE_ID_KIRBY_SWORD_ANIM, {18, 69}}}
 };
 
 const unsigned int NUM_ABILITY_ANIMATIONS[] =
