ab71c862f5b4eb96ed222b0541fe215c1fa3ab60
Benjamin Paul <bepaul@microsoft.com>
2025-08-18 16:53:27 -0400

    Chalk: Don't draw steps if step-goal reached

--- src/chalk/stepsLayer.c
+++ src/chalk/stepsLayer.c
@@ -7,13 +7,16 @@ bool g_fStepGoalMet;
 
 void steps_layer_update_callback(Layer* layer, GContext* ctx)
 {
-	int stepsPct = (g_settings.stepsGoal - g_steps) * 100 / g_settings.stepsGoal;
-	graphics_context_set_stroke_color(ctx, GColorDarkCandyAppleRed);
-	graphics_context_set_stroke_width(ctx, 7);
+	if (g_steps < g_settings.stepsGoal)
+	{
+		int stepsPct = (g_settings.stepsGoal - g_steps) * 100 / g_settings.stepsGoal;
+		graphics_context_set_stroke_color(ctx, GColorDarkCandyAppleRed);
+		graphics_context_set_stroke_width(ctx, 7);
 
-	int32_t wedgeAngle =  stepsPct * 9 * TRIG_MAX_ANGLE / (10 * 360);
-	int32_t wedgeMiddle = TRIG_MAX_ANGLE * 1/4;
-	graphics_draw_arc(ctx, GRect(-90,1,178,178), GOvalScaleModeFillCircle, wedgeMiddle-wedgeAngle, wedgeMiddle+wedgeAngle);
+		int32_t wedgeAngle =  stepsPct * 9 * TRIG_MAX_ANGLE / (10 * 360);
+		int32_t wedgeMiddle = TRIG_MAX_ANGLE * 1/4;
+		graphics_draw_arc(ctx, GRect(-90,1,178,178), GOvalScaleModeFillCircle, wedgeMiddle-wedgeAngle, wedgeMiddle+wedgeAngle);
+	}
 }
 
 void load_steps_layer(Layer* parent_layer)
