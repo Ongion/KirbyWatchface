a03a043716421815fb56e540804623ed60ee79a3
Benjamin Paul <bepaul@microsoft.com>
2026-05-15 18:48:05 -0400

    Remove axis requirement for accel-tap event
    Set default weather source to OPENMETEO
    
    Bump version number

--- src/common/main.c
+++ src/common/main.c
@@ -1125,25 +1125,22 @@ static void show_date_timer_handler(void* context)
 
 static void handle_tap(AccelAxisType axis, int32_t direction)
 {
-	if (axis == ACCEL_AXIS_Y)
+	// Cancel existing showDate timer
+	if (s_pShowDateTimer)
 	{
-		// Cancel existing showDate timer
-		if (s_pShowDateTimer)
-		{
-			app_timer_cancel(s_pShowDateTimer);
-		}
-
-		if (!s_pKirbyAnimationTimer)
-		{
-			load_and_play_ability_animation(get_random_ability_animation());
-		}
-
-		light_enable_interaction();
-		layer_set_hidden(text_layer_get_layer(s_pTextLayerDate), true);
-		layer_set_hidden(text_layer_get_layer(s_pTextLayerDayOfWeek), true);
-		layer_set_hidden(text_layer_get_layer(s_pTextLayerWeather), false);
-		s_pShowDateTimer = app_timer_register(2000, show_date_timer_handler, NULL);
+		app_timer_cancel(s_pShowDateTimer);
 	}
+
+	if (!s_pKirbyAnimationTimer)
+	{
+		load_and_play_ability_animation(get_random_ability_animation());
+	}
+
+	light_enable_interaction();
+	layer_set_hidden(text_layer_get_layer(s_pTextLayerDate), true);
+	layer_set_hidden(text_layer_get_layer(s_pTextLayerDayOfWeek), true);
+	layer_set_hidden(text_layer_get_layer(s_pTextLayerWeather), false);
+	s_pShowDateTimer = app_timer_register(2000, show_date_timer_handler, NULL);
 }
 
 static void unobstructed_area_will_change_handler(GRect final_unobstructed_screen_area, void* context)
@@ -1232,7 +1229,7 @@ static void main_window_unload(Window* window)
 
 static void load_settings()
 {
-	g_settings.weatherSource = OPENWEATHER;
+	g_settings.weatherSource = OPENMETEO;
 	g_settings.openWeatherMapAPIKey[0] = '\0';
 	g_settings.city[0] = '\0';
 	g_settings.scalePreference = FAHRENHEIT;
